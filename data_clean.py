a97fb9c84c1f8436b214b19e4cdc492f406c19f1
JustinUhr <justin_uhr@brown.edu>
2023-06-22 15:08:04 -0400

    first pass at categorical encoding

--- data_clean.py
+++ data_clean.py
@@ -25,6 +25,11 @@ import json
 import pprint
 
 import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+from sklearn.model_selection import train_test_split
+
 
 def load_data():
     # Get the docs from the raw json file
@@ -45,17 +50,71 @@ def df_to_dataset(df, shuffle=True, batch_size=32):
     ds = ds.prefetch(batch_size)
     return ds
 
+def train_val_test_split(df, test_size=0.1):
+    # split the data into train, validation, and test sets
+    train, test = train_test_split(df, test_size)
+    train, val = train_test_split(train, test_size)
+    batch_size = 64
+    train_ds = df_to_dataset(train, batch_size=batch_size)
+    val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+    test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+    return train_ds, val_ds, test_ds
 
-if __name__ == '__main__':
+def get_category_encoding_layer(name, dataset, dtype, max_tokens=None):
+    # Create a StringLookup layer which will turn strings into integer indices
+    if dtype == 'string':
+        index = preprocessing.StringLookup(max_tokens=max_tokens)
+    else:
+        index = preprocessing.IntegerLookup(max_tokens=max_tokens)
+
+    # Prepare a Dataset that only yields our feature
+    feature_ds = dataset.map(lambda x, y: x[name])
+
+    # Learn the set of possible values and assign them a fixed integer index.
+    index.adapt(feature_ds)
+
+    # Create a Discretization for our integer indices.
+    encoder = preprocessing.CategoryEncoding(num_tokens=index.vocabulary_size())
+
+    # Apply one-hot encoding to our indices and return this feature
+    return lambda feature: encoder(index(feature))
+
+
+
+def encode_categorical_columns(all_inputs, encoded_features, categorical_columns):
+    # Convert categorical columns to numeric
+    for column in categorical_columns:
+        categorical_column = tf.keras.Input(shape=(1,), name=column, dtype='string')
+        encoding_layer = get_category_encoding_layer(column, train_ds, dtype='string', max_tokens=5)
+        encoded_categorical_column = encoding_layer(categorical_column)
+        all_inputs.append(categorical_column)
+        encoded_features.append(encoded_categorical_column)
+    return encoded_features
+
+def manager():
+    # load the data
     df = load_data()
-    # # print a list of the unique values in the mods_subject_broad_theme_ssim column
-    # broad_themes = df['mods_subject_broad_theme_ssim'].dropna().values
-    # broad_themes = [item for sublist in broad_themes for item in sublist]
-    # for i in sorted(set(broad_themes)):
-    #     print(i)
 
-# create a copy of the dataframe with only the columns we want to use
+    # create a copy of the dataframe with only the columns we want to use
     df = df[['pid','genre','keyword', 'mods_location_physical_location_ssim', 'mods_language_code_ssim', 'mods_subject_broad_theme_ssim']]
+    
     # drop rows with no values in the mods_subject_broad_theme_ssim column
     df = df.dropna(subset=['mods_subject_broad_theme_ssim'])
+
+    # split the data into train, validation, and test sets
+    train_ds, val_ds, test_ds = train_val_test_split(df, test_size=0.1)
     
+    # Define the categorical columns
+    categorical_columns = ['mods_location_physical_location_ssim', 'mods_language_code_ssim', 'mods_subject_broad_theme_ssim']
+
+    # Encode the categorical columns
+    all_inputs = []
+    encoded_features = []
+    encoded_features = encode_categorical_columns(all_inputs, encoded_features, categorical_columns)
+
+    # Print the encoded features to see what they look like
+    print('Encoded Features:')
+    pprint.pprint(encoded_features)
+
+if __name__ == '__main__':
+    manager()
