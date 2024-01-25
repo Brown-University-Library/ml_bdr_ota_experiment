fa8b32c2d2657a5ae1326978d27453c27ae4a45e
JustinUhr <justin_uhr@brown.edu>
2024-01-25 12:06:27 -0500

    Add model evaluation function and update requirements.txt

--- ml_mastery_tutorial/main_script.py
+++ ml_mastery_tutorial/main_script.py
@@ -27,9 +27,43 @@ for i in range(10):
 	print(X[i], y[i])
 	
 
+# get the model
+# define a function to get the model
+def get_model(n_inputs, n_outputs):
+	# create a sequential model
+	model = Sequential()
+	# add a dense layer with 20 units, using 'relu' activation function
+	model.add(Dense(20, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
+	# add a dense layer with n_outputs units, using 'sigmoid' activation function
+	model.add(Dense(n_outputs, activation='sigmoid'))
+	# compile the model with binary cross-entropy loss and adam optimizer
+	model.compile(loss='binary_crossentropy', optimizer='adam')
+	return model
 
-# define the model
-model = Sequential()
-model.add(Dense(20, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
-model.add(Dense(n_outputs, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam')
+# evaluate a model using repeated k-fold cross-validation
+def evaluate_model(X, y):
+	results = list()
+	n_inputs, n_outputs = X.shape[1], y.shape[1]
+	# define evaluation procedure
+	cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+	# enumerate folds
+	for train_ix, test_ix in cv.split(X):
+		#prepare data
+		X_train, X_test = X[train_ix], X[test_ix]
+		y_train, y_test = y[train_ix], y[test_ix]
+		# define model
+		model = get_model(n_inputs, n_outputs)
+		# fit model
+		model.fit(X_train, y_train, verbose=0, epochs=100)
+		# make a prediction on the test set
+		yhat = model.predict(X_test)
+		# round probabilities to class labels
+		yhat = yhat.round()
+		# calculate accuracy
+		acc = accuracy_score(y_test, yhat)
+		# store result
+		print('>%.3f' % acc)
+		results.append(acc)
+	return results
+
+STOP HERE!!!
